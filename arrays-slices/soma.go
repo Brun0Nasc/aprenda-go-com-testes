26c61998c2b1ff88df6e73939322521a579f2307
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-10-03 11:53:41 -0300

    somaTudo, somando slices

--- arrays-slices/soma.go
+++ arrays-slices/soma.go
@@ -7,3 +7,14 @@ func Soma(numeros []int) (soma int) {
 	}
 	return
 }
+
+func SomaTudo(numerosParaSomar ...[]int) (somas []int) {
+	quantidadeDeNumeros := len(numerosParaSomar)
+	somas = make([]int, quantidadeDeNumeros) //* O make permite criar um slice com uma capacidade inicial de *len* *numerosParaSomar* que precisamos percorrer 
+
+	for i, numeros := range numerosParaSomar {
+		somas[i] = Soma(numeros)
+	}
+
+	return
+}
--- arrays-slices/soma_test.go
+++ arrays-slices/soma_test.go
@@ -1,6 +1,9 @@
 package arraysslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSoma(t *testing.T) {
 
@@ -15,4 +18,13 @@ func TestSoma(t *testing.T) {
 		}
 	})
 
+}
+
+func TestSomaTudo(t *testing.T) {
+	resultado := SomaTudo([]int{1, 2}, []int{0, 9})
+	esperado := []int{3, 9}
+
+	if !reflect.DeepEqual(resultado, esperado) {
+		t.Errorf("Resultado '%v', esperado '%v'", resultado, esperado)
+	}
 }
