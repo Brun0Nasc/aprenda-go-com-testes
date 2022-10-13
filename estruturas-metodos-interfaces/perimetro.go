50528c7c5eb4be1965481b2f12e8ec709e5a01f5
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-10-13 08:44:56 -0300

    Adicionando comentarios

--- estruturas-metodos-interfaces/perimetro.go
+++ estruturas-metodos-interfaces/perimetro.go
@@ -2,36 +2,36 @@ package estruturasmetodosinterfaces
 
 import "math"
 
-type Forma interface {
+type Forma interface { //* Interface que recebe uma função Area do tipo float64
 	Area() float64
 }
 
-type Retangulo struct {
+type Retangulo struct { //* Estrutura de retangulo que recebe largura e altura
 	Largura float64
 	Altura  float64
 }
 
-type Circulo struct {
+type Circulo struct { //* Estrutura de círculo que recebe raio
 	Raio float64
 }
 
-type Triangulo struct {
+type Triangulo struct { //* Estrutura de triangulo que recebe base e altura
 	Base   float64
 	Altura float64
 }
 
-func (r Retangulo) Perimetro() float64 {
+func (r Retangulo) Perimetro() float64 { //* Método para calcular o perímetro de um retangulo
 	return 2 * (r.Largura + r.Altura)
 }
 
-func (r Retangulo) Area() float64 {
+func (r Retangulo) Area() float64 { //* Método para calcular a area do retangulo, a função área desse método é o que a interface requer
 	return r.Largura * r.Altura
 }
 
-func (c Circulo) Area() float64 {
+func (c Circulo) Area() float64 { //* Método para calcular area do circulo, nos mesmos moldes do método de retângulo
 	return math.Pi * c.Raio * c.Raio
 }
 
-func (t Triangulo) Area() float64 {
+func (t Triangulo) Area() float64 { //* Método para calcular a area do triangulo
 	return (t.Base * t.Altura) * 0.5
 }
--- estruturas-metodos-interfaces/perimetro_test.go
+++ estruturas-metodos-interfaces/perimetro_test.go
@@ -2,7 +2,7 @@ package estruturasmetodosinterfaces
 
 import "testing"
 
-func TestAreaPerimetro(t *testing.T) {
+func TestAreaPerimetro(t *testing.T) { //* testando a função de calcular perímetro da forma normal
 
 	retangulo := Retangulo{10.0, 10.0}
 	resultado := retangulo.Perimetro()
@@ -14,20 +14,22 @@ func TestAreaPerimetro(t *testing.T) {
 
 }
 
-func TestArea(t *testing.T) {
-	testesArea := []struct {
-		nome string
-		forma Forma
-		temArea float64
+func TestArea(t *testing.T) { //* testando os metodos de calcular area das formas
+	testesArea := []struct { //* criando uma struct para receber structs das formas
+		nome string //* o campo nome vai servir para nomear os testes que serão feitos
+		forma Forma //* forma vai receber a interface de forma que se refere a cada estrutura de forma declarada no outro arquivo
+		temArea float64 //* campo para receber a área que espera-se obter no calculo da função area
 	} {
-		{nome: "Retângulo", forma: Retangulo{Largura: 12, Altura: 6}, temArea:72.0},
+		{nome: "Retângulo", forma: Retangulo{Largura: 12, Altura: 6}, temArea:72.0}, //* passando cada forma com nome, forma e area esperada
 		{nome: "Círculo", forma: Circulo{Raio: 10}, temArea: 314.1592653589793},
 		{nome: "Triângulo", forma: Triangulo{Base: 12, Altura: 6}, temArea: 36.0},
 	}
 
-	for _, tt := range testesArea {
-		t.Run(tt.nome, func(t *testing.T) {
-			resultado := tt.forma.Area()
+	for _, tt := range testesArea { //* o for vai percorrer o testesArea para testar cada estrutura declarada unitariamente
+		t.Run(tt.nome, func(t *testing.T) { //* o t.Run vai organizar melhor o teste, exibindo o nome de cada teste
+			//* isso é útil pois se um arquivo de testes tiver muitos testes a serem feitos, o log vai aparecer nomeado e será mais fácil encontrar no código
+			resultado := tt.forma.Area() //* tt é cada elemento único que está sendo iterado dentro de testesArea, forma é o campo que recebe Forma
+			//* a partir do que é recebido nesse campo, estruturas singulares, é possível chamar as funções dos métodos declarados para cada forma
 			if resultado != tt.temArea {
 				t.Errorf("resultado %.2f esperado %.2f", resultado, tt.temArea)
 			}	
