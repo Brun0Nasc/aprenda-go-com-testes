c1d7f94a71a99c6d8e0b4c3b659d0a17cfaeb9e6
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-10-13 11:20:24 -0300

    funcao retirar

--- ponteiros-e-erros/carteira.go
+++ ponteiros-e-erros/carteira.go
@@ -1,13 +1,29 @@
 package ponteiroseerros
 
+import "fmt"
+
+type Bitcoin int
+
 type Carteira struct {
-	saldo int
+	saldo Bitcoin
 }
 
-func (c *Carteira) Depositar(valor int) {
+type Stringer interface {
+	String() string
+}
+
+func (b Bitcoin) String() string {
+	return fmt.Sprintf("%d BTC", b)
+}
+
+func (c *Carteira) Depositar(valor Bitcoin) {
 	c.saldo += valor
 }
 
-func (c *Carteira) Saldo() int {
+func (c *Carteira) Saldo() Bitcoin {
 	return c.saldo
+}
+
+func (c *Carteira) Retirar(valor Bitcoin) {
+	c.saldo -= valor
 }
--- ponteiros-e-erros/carteira_test.go
+++ ponteiros-e-erros/carteira_test.go
@@ -5,15 +5,36 @@ import (
 )
 
 func TestCarteira(t *testing.T) {
-	carteira := Carteira{}
 
-	carteira.Depositar(10)
-
-	resultado := carteira.Saldo()
-
-	esperado := 10
-
-	if resultado != esperado {
-		t.Errorf("resultado %d, esperado %d", resultado, esperado)
+	verificaTeste := func (t *testing.T, resultado, esperado Bitcoin)  {
+		t.Helper()
+		if resultado != esperado {
+			t.Errorf("resultado %s, esperado %s", resultado, esperado)
+		}
 	}
+
+	t.Run("Depositar", func(t *testing.T) {
+		carteira := Carteira{}
+
+		carteira.Depositar(Bitcoin(10))
+
+		resultado := carteira.Saldo()
+
+		esperado := Bitcoin(10)
+
+		verificaTeste(t, resultado, esperado)
+	})
+
+	t.Run("Retirar", func(t *testing.T) {
+		carteira := Carteira{saldo: Bitcoin(20)}
+
+		carteira.Retirar(Bitcoin(10))
+
+		resultado := carteira.Saldo()
+
+		esperado := Bitcoin(10)
+
+		verificaTeste(t, resultado, esperado)
+	})
+
 }
