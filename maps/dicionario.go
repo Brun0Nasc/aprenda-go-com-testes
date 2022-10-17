945ba004515ede84ab53318c8062dfa2c55bc066
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-10-17 08:28:56 -0300

    Testando a existencia de palavra no dicionario

--- maps/dicionario.go
+++ maps/dicionario.go
@@ -1,7 +1,14 @@
 package maps
 
+import "errors"
+
 type Dicionario map[string]string
 
-func (d Dicionario) Busca(palavra string) string{
-	return d[palavra]
+func (d Dicionario) Busca(palavra string) (string, error){
+	definicao, existe := d[palavra] //* o segundo valor recebe um boolean que informa se a palavra informada existe ou não
+	if !existe {
+		return "", errors.New("não foi possivel encontrar a palavra que voce procura")
+	}
+
+	return definicao, nil
 }
--- maps/dicionario_test.go
+++ maps/dicionario_test.go
@@ -3,12 +3,23 @@ package maps
 import "testing"
 
 func TestBusca(t *testing.T) {
-	dicionario := Dicionario{"teste":"isso é apenas um teste"}
+	dicionario := Dicionario{"teste": "isso é apenas um teste"}
 
-	resultado := dicionario.Busca("teste")
-	esperado := "isso é apenas um teste"
+	t.Run("palavra conhecida", func(t *testing.T) {
+		resultado, _ := dicionario.Busca("teste")
+		esperado := "isso é apenas um teste"
 
-	comparaStrings(t, resultado, esperado)
+		comparaStrings(t, resultado, esperado)
+	})
+
+	t.Run("palavra desconhecida", func(t *testing.T) {
+		_, err := dicionario.Busca("desconhecida")
+
+		if err == nil {
+			t.Fatal("é esperado que um erro seja obtido")
+		}
+	})
+
 }
 
 func comparaStrings(t *testing.T, resultado, esperado string) {
