fe6080d5fcf82cc1edc96b83e0b6b3a91a1c0e5a
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-10-19 09:41:52 -0300

    refatorando para ter uma interface de proposito geral na função

--- mocks/contagem.go
+++ mocks/contagem.go
@@ -6,10 +6,10 @@ import (
 	"os"
 )
 
-func Contagem(saida io.Writer){
+func Contagem(saida io.Writer){ //* io.Writer é a interface que vai receber uma fatia de bytes que vai ser mandada para qualquer saída especificada
 	fmt.Fprintf(saida, "3")
 }
 
 func main(){
-	Contagem(os.Stdout)
+	Contagem(os.Stdout) //* os.Stdout vai definir que a saída desse resultado vai ser no Terminal
 }
--- mocks/contagem_test.go
+++ mocks/contagem_test.go
@@ -6,7 +6,7 @@ import (
 )
 
 func TestContagem(t *testing.T) {
-	buffer := &bytes.Buffer{}
+	buffer := &bytes.Buffer{} //* Um Buffer é um buffer de bytes de tamanho variável com métodos Read e Write. O valor zero para Buffer é um buffer vazio pronto para uso.
 
 	Contagem(buffer)
 
