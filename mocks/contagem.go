ffbf2736f2f5577124d2d87197cad0e3e2873027
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-10-19 09:46:39 -0300

    iterando contagem regressiva

--- mocks/contagem.go
+++ mocks/contagem.go
@@ -6,10 +6,13 @@ import (
 	"os"
 )
 
-func Contagem(saida io.Writer){ //* io.Writer é a interface que vai receber uma fatia de bytes que vai ser mandada para qualquer saída especificada
-	fmt.Fprintf(saida, "3")
+func Contagem(saida io.Writer) { //* io.Writer é a interface que vai receber uma fatia de bytes que vai ser mandada para qualquer saída especificada
+	for i := 3; i > 0; i-- {
+		fmt.Fprintln(saida, i)
+	}
+	fmt.Fprint(saida, "Vai!")
 }
 
-func main(){
+func main() {
 	Contagem(os.Stdout) //* os.Stdout vai definir que a saída desse resultado vai ser no Terminal
 }
