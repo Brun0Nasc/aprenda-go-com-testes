fd1652b599c592a9ba60c57c9065c90dc534aab7
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-11-03 11:02:08 -0300

    reflexao v10.1 - refatoração

--- reflexao/reflexao.go
+++ reflexao/reflexao.go
@@ -5,46 +5,27 @@ import "reflect"
 func percorre(x interface{}, fn func(entrada string)) {
 	valor := obtemValor(x)
 
-	quantidadeDeValores := 0
-	var obtemCampo func(int) reflect.Value
+	percorreValor := func(valor reflect.Value) {
+		percorre(valor.Interface(), fn)
+	}
 
-	switch valor.Kind(){
+	switch valor.Kind() {
 	case reflect.String:
 		fn(valor.String())
 	case reflect.Struct:
-		quantidadeDeValores = valor.NumField()
-		obtemCampo = valor.Field
+		for i := 0; i < valor.NumField(); i++ {
+			percorreValor(valor.Field(i))
+		}
 	case reflect.Slice, reflect.Array:
-		quantidadeDeValores = valor.Len()
-		obtemCampo = valor.Index
+		for i := 0; i < valor.Len(); i++ {
+			percorreValor(valor.Field(i))
+		}
 	case reflect.Map:
 		for _, chave := range valor.MapKeys() {
-			percorre(valor.MapIndex(chave).Interface(), fn)
+			percorreValor(valor.MapIndex(chave))
 		}
 	}
 
-	for i := 0; i < quantidadeDeValores; i++ {
-		percorre(obtemCampo(i).Interface(), fn)
-	}
-
-	// if valor.Kind() == reflect.Slice {
-	// 	for i := 0; i < valor.Len(); i++ {
-	// 		percorre(valor.Index(i).Interface(), fn)
-	// 	}
-	// 	return
-	// }
-
-	// for i := 0; i < valor.NumField(); i++ {
-	// 	campo := valor.Field(i)
-
-	// 	switch campo.Kind() {
-	// 	case reflect.String:
-	// 		fn(campo.String())
-	// 	case reflect.Struct:
-	// 		percorre(campo.Interface(), fn)
-	// 	}
-
-	// }
 }
 
 func obtemValor(x interface{}) reflect.Value {
