afe568cadaf8770c6b56b9af86e314f1cc50c0f3
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-11-03 09:35:19 -0300

    reflexao v3

--- reflexao/reflexao_test.go
+++ reflexao/reflexao_test.go
@@ -1,25 +1,55 @@
 package reflexao
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestPercorre(t *testing.T) {
 
-	esperado := "Chris"
-	var resultado []string
-
-	x := struct {
-		Nome string
-	}{esperado}
-
-	percorre(x, func(entrada string) {
-		resultado = append(resultado, entrada)
-	})
-
-	if len(resultado) != 1 {
-		t.Errorf("número incorreto de chamadas de função: resultado %d, esperado %d", len(resultado), 1)
+	casos := []struct {
+		Nome              string
+		Entrada           interface{}
+		ChamadasEsperadas []string
+	}{
+		{
+			"Structs com um campo string",
+			struct {
+				Nome string
+			}{"Chris"},
+			[]string{"Chris"},
+		},
 	}
 
-	if resultado[0] != esperado {
-		t.Errorf("resultado '%v', esperado '%v'", resultado[0], esperado)
+	for _, teste := range casos {
+		t.Run(teste.Nome, func(t *testing.T) {
+			var resultado []string
+			percorre(teste.Entrada, func(entrada string) {
+				resultado = append(resultado, entrada)
+			})
+
+			if !reflect.DeepEqual(resultado, teste.ChamadasEsperadas){
+				t.Errorf("resultado '%v', esperado '%v'", resultado, teste.ChamadasEsperadas)
+			}
+		})
 	}
+
+	// esperado := "Chris"
+	// var resultado []string
+
+	// x := struct {
+	// 	Nome string
+	// }{esperado}
+
+	// percorre(x, func(entrada string) {
+	// 	resultado = append(resultado, entrada)
+	// })
+
+	// if len(resultado) != 1 {
+	// 	t.Errorf("número incorreto de chamadas de função: resultado %d, esperado %d", len(resultado), 1)
+	// }
+
+	// if resultado[0] != esperado {
+	// 	t.Errorf("resultado '%v', esperado '%v'", resultado[0], esperado)
+	// }
 }
