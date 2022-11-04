fb1d5d86d0cd82a0fc466cd914226919f5377c66
Bruno do Nascimento de Brito <104685758+Brun0Nasc@users.noreply.github.com>
2022-11-04 09:14:58 -0300

    waitgroup, mutex lock e unlock

--- sync/sincronia.go
+++ sync/sincronia.go
@@ -1,10 +1,19 @@
 package sync
 
+import "sync"
+
 type Contador struct {
+	mu    sync.Mutex
 	valor int
 }
 
+/*
+	*Um Mutex é uma trava de exclusão mútua. O valor zero de um Mutex é um Mutex destravado.
+*/
+
 func (c *Contador) Incrementa() {
+	c.mu.Lock()
+	defer c.mu.Unlock()
 	c.valor++
 }
 
--- sync/sincronia_test.go
+++ sync/sincronia_test.go
@@ -1,16 +1,50 @@
 package sync
 
-import "testing"
+import (
+	"sync"
+	"testing"
+)
 
 func TestContador(t *testing.T) {
+	verificaContador := func(t *testing.T, resultado *Contador, esperado int) {
+		t.Helper()
+		if resultado.Valor() != esperado {
+			t.Errorf("resultado %d, esperado %d", resultado.Valor(), esperado)
+		}
+	}
+
 	t.Run("incrementar o contador 3 vezes resulta no valor 3", func(t *testing.T) {
 		contador := Contador{}
 		contador.Incrementa()
 		contador.Incrementa()
 		contador.Incrementa()
 
-		if contador.Valor() != 3 {
-			t.Errorf("resultado %d, esperado %d", contador.Valor(), 3)
+		verificaContador(t, &contador, 3)
+	})
+
+	t.Run("roda concorrentemente em segurança", func(t *testing.T) {
+		contagemEsperada := 1000
+		contador := Contador{}
+
+		var wg sync.WaitGroup
+		wg.Add(contagemEsperada)
+
+		for i := 0; i < contagemEsperada; i++ {
+			go func(w *sync.WaitGroup) {
+				contador.Incrementa()
+				w.Done()
+			}(&wg)
 		}
+		wg.Wait()
+
+		verificaContador(t, &contador, contagemEsperada)
+
+		/*
+			*Um WaitGroup aguarda por uma coleção de goroutines terminar seu processamento. 
+			*A goroutine principal faz a chamada para o Add definir o número de goroutines 
+			*que serão esperadas. Então, cada uma das goroutines é executada e chama Done 
+			*quando termina sua execução. Ao mesmo tempo, Wait pode ser usado para bloquear
+			*a execução até que todas as goroutines tenham terminado.
+		*/
 	})
 }
